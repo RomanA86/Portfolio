b00338ca44f2b66f41127bd6fa263bf8475c46e5
Roman <alvarezroman150@gmail.com>
2022-11-14 12:07:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const icon = document.getElementById('icon-light-mode');
+const flagsElement = document.getElementById('flags');
+const textsToChange = document.querySelectorAll('[data-section]');
+
+const changeLanguage = async language =>{
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+    //console.log(texts)
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+}
+
+icon.addEventListener('click', ()=>{
+    document.body.classList.toggle('light-mode');
+
+    //Guardamos el modo en localstorage
+    if(document.body.classList.contains('light-mode')){
+        localStorage.setItem('light-mode', 'true');
+        icon.src = './assets/bxs-moon.svg';
+    } else {
+        localStorage.setItem('light-mode', 'false');
+        icon.src = './assets/bxs-sun.svg';
+    }
+})
+
+flagsElement.addEventListener('click',(e)=>{
+    const lang = e.target.parentElement.dataset.language;
+    changeLanguage(lang);
+    switch (lang) {
+        case 'es':
+            localStorage.setItem('language', 'espaniol')
+            break;
+        case 'en':
+            localStorage.setItem('language', 'english')
+        break;
+    }
+    
+});
+
+//Obtenemos el modo actual
+if(localStorage.getItem('light-mode') === 'true'){
+    document.body.classList.add('light-mode');
+    icon.src = './assets/bxs-moon.svg';
+} else {
+    document.body.classList.remove('light-mode');
+    icon.src = './assets/bxs-sun.svg';
+}
+
+if(localStorage.getItem('language') === 'espaniol') {
+    changeLanguage('es');
+}
+if(localStorage.getItem('language') === 'english') {
+    changeLanguage('en');
+}
